5310d2ea01520ae2713991875b0a8f4cead6912f
Ryan24Price <illusion165@hotmail.com>
2024-03-06 16:30:11 -0500

    inital commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:19-alpine
+WORKDIR /app
+COPY . .
+EXPOSE 8000
+RUN npm install
+CMD ["npm", "start"]
+
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.use(express.json())
+
+app.use("/", (req, res) => {
+    res.send({ status: 200, message: "Hello World, Module 10 Labs"})
+})
+
+app.listen(port, ()=> {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
